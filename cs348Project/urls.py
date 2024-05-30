20f2117490183a7aa28d50df6b97371f757070f6
Benjamin Ng <60500272+benymng@users.noreply.github.com>
2024-05-29 21:47:25 -0400

    Create new purchase endpoint

--- cs348Project/urls.py
+++ cs348Project/urls.py
@@ -2,7 +2,23 @@ from django.contrib import admin
 from django.urls import path
 from django.db import connection
 from django.http import JsonResponse
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Schema
+from typing import List
+
+
+class PurchaseSplit(Schema):
+    amount: int
+    borrower: int
+
+
+class Purchase(Schema):
+    name: str
+    category: str
+    group_id: int
+    total_cost: int
+    purchaser: int
+    purchase_splits: List[PurchaseSplit]
+
 
 api = NinjaAPI()
 
@@ -20,9 +36,52 @@ def home(request):
 @api.get("/users")
 def get_users(request):
     with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM test")
+        cursor.execute("SELECT * FROM multiset_user")
         rows = cursor.fetchall()
     return JsonResponse(rows, safe=False)
+
+
+@api.post("/new-purchase")
+def new_purchase(request, purchase: Purchase):
+    """
+    Creates a new purchase and its splits in the database.
+    Args:
+        purchase: the purchase object to be created and a list of purchase splits
+    Returns:
+        a JSON response with the status of the operation
+    """
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """
+                INSERT INTO purchase (name, category, group_id, total_cost, purchaser)
+                VALUES (%s, %s, %s, %s, %s)
+            """,
+            [
+                purchase.name,
+                purchase.category,
+                purchase.group_id,
+                purchase.total_cost,
+                purchase.purchaser,
+            ],
+        )
+
+        purchase_id = cursor.lastrowid
+        if not purchase_id:
+            return JsonResponse({"status": "error, purchase_id not found"})
+
+        for purchase_split in purchase.purchase_splits:
+            cursor.execute(
+                """
+                    INSERT INTO purchase_splits (purchase_id, amount, borrower)
+                    VALUES (%s, %s, %s)
+                """,
+                [
+                    purchase_id,
+                    purchase_split.amount,
+                    purchase_split.borrower,
+                ],
+            )
+    return JsonResponse({"status": "success"})
 
 
 urlpatterns = [
